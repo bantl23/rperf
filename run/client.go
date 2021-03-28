bef0ea02720ad7656ee51c20ba41fa3485469a5a
Deepak Kaul <deepak.b.kaul@gmail.com>
2021-03-28 05:51:25 +0000

    adding in stats

--- run/client.go
+++ run/client.go
@@ -19,7 +19,7 @@ func (client *Client) Run() error {
 	buffer := make([]byte, client.Size)
 
 	connectChans := make([]chan struct{}, 0)
-	statsChan := make(chan Stats, 10000)
+	statsChan := make([]chan Stats, 0)
 	beginChans := make([]chan struct{}, 0)
 	endChans := make([]chan struct{}, 0)
 
@@ -29,11 +29,13 @@ func (client *Client) Run() error {
 		for j := 0; j < client.Connections; j++ {
 			connectChan := make(chan struct{}, 1)
 			beginChan := make(chan struct{}, 1)
+			statChan := make(chan Stats, 1)
 			endChan := make(chan struct{}, 1)
 			wg.Add(1)
-			go clientRunTcp(client.Addresses[i], j, buffer, connectChan, statsChan, beginChan, endChan, wg)
+			go clientRunTcp(client.Addresses[i], j, buffer, connectChan, statChan, beginChan, endChan, wg)
 			connectChans = append(connectChans, connectChan)
 			beginChans = append(beginChans, beginChan)
+			statsChan = append(statsChan, statChan)
 			endChans = append(endChans, endChan)
 		}
 	}
@@ -42,6 +44,8 @@ func (client *Client) Run() error {
 	for i := range connectChans {
 		<-connectChans[i]
 	}
+
+	fmt.Printf("CLIENT: ALL CONNECTED\n")
 
 	// start go routines
 	for i := range beginChans {
@@ -56,6 +60,26 @@ func (client *Client) Run() error {
 	}
 
 	wg.Wait()
+
+	totals := make(map[string]*Stats)
+	// calculate bandwidth
+	for i := range statsChan {
+		s := <-statsChan[i]
+		_, ok := totals[s.Address]
+		if !ok {
+			totals[s.Address] = &Stats{
+				Address:     s.Address,
+				Bytes:       s.Bytes,
+				ElapsedTime: s.ElapsedTime,
+			}
+			totals[s.Address].Bytes = totals[s.Address].Bytes + s.Bytes
+			totals[s.Address].ElapsedTime = totals[s.Address].ElapsedTime + s.ElapsedTime
+		}
+	}
+	for k := range totals {
+		mbps := float64(totals[k].Bytes) * 8 / 1024 / 1024 / totals[k].ElapsedTime.Seconds()
+		fmt.Printf("CLIENT AVG [%s]: %f Mbps\n", totals[k].Address, mbps)
+	}
 
 	return nil
 }
@@ -74,6 +98,8 @@ func clientRunTcp(address string, item int, buffer []byte, connected chan<- stru
 
 	connected <- struct{}{} // notify connected
 	<-beg                   // block until all routines are connected
+
+	fmt.Printf("CLIENT %s: STARTING %s [%+v]\n", conn.LocalAddr(), conn.RemoteAddr(), time.Now())
 
 	totalBytes := uint64(0)
 	totalElapsed := time.Duration(0)
@@ -96,6 +122,12 @@ func clientRunTcp(address string, item int, buffer []byte, connected chan<- stru
 			done = true
 		default:
 		}
+	}
+	stats <- Stats{
+		Address:     conn.RemoteAddr().String(),
+		Item:        item,
+		Bytes:       totalBytes,
+		ElapsedTime: totalElapsed,
 	}
 
 	mbps := float64(totalBytes) * 8 / 1024 / 1024 / totalElapsed.Seconds()
